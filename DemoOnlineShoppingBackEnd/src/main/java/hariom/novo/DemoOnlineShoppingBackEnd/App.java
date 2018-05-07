77ad03b8f4c8bb6fcb0e7ba168b92b29ef1d8e07
harry-novo <hariom-novo@gmail.com>
2018-05-07 16:41:15 +0530

    My project Setup is completed Successfully.

---
+++ DemoOnlineShoppingBackEnd/src/main/java/hariom/novo/DemoOnlineShoppingBackEnd/App.java
@@ -0,0 +1,13 @@
+package hariom.novo.DemoOnlineShoppingBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
